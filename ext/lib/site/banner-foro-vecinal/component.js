c75e4f15b218fd7679957781c0ad8c819345411a
nigmata <fchiari@hotmail.com>
2017-07-01 19:35:34 -0300

    Corregir titulo de foros vecinales y cambiar imagen del mapa en el footer

--- ext/lib/site/banner-foro-vecinal/component.js
+++ ext/lib/site/banner-foro-vecinal/component.js
@@ -7,13 +7,14 @@ export default function BannerForoVecinal (props) {
   return (
     <section className="seccion-general">
       <div className="banner">
-        <div
-          className='contenedor'>
-            <div
-              className='fondo-titulo'>
-                <h1>FOROS VECINALES</h1>
-            </div>
-        </div>
+
+      </div>
+      <div
+        className='contenedor'>
+          <div
+            className='fondo-titulo center-block'>
+              <h1>FOROS VECINALES</h1>
+          </div>
       </div>
     </section>
   )
