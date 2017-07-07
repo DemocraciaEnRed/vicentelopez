ca6db1303152f4e994ea031b32eb28d85d5934ae
Valentin <valentinmuro@gmail.com>
2017-07-07 15:59:39 -0300

    Corrige static pages

--- ext/lib/site/boot/overrides.js
+++ ext/lib/site/boot/overrides.js
@@ -1,7 +1,6 @@
 /**
  * Override Forum Home with a custom one located at /ext
  */
-
 import * as HomeForum from 'lib/site/home-forum/component'
 import HomeForumExt from 'ext/lib/site/home-forum/component'
 
--- ext/lib/site/static-pages/static-pages.js
+++ ext/lib/site/static-pages/static-pages.js
@@ -1,14 +1,8 @@
-import React from 'react'
-import {Route} from 'react-router'
 import 'lib/boot/routes'
 import router from 'lib/site/boot/router'
 import TerminosYCondiciones from './pages/terminos-y-condiciones'
 
-const site = router.find((route) => route.key === 'lib-site')
-
-site.props.children.unshift(
-  <Route
-    key='ext-terminos-y-condiciones'
-    path='/s/terminos-y-condiciones'
-    component={TerminosYCondiciones} />
-)
+router.childRoutes.unshift({
+  path: 's/terminos-y-condiciones',
+  component: TerminosYCondiciones
+})
