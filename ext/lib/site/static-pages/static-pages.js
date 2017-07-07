5e06ad5c98b00d4df03e87334f7d9609c88332d1
Valentin <valentinmuro@gmail.com>
2017-07-07 15:23:24 -0300

    Agrega static-pages

--- ext/lib/site/static-pages/static-pages.js
+++ ext/lib/site/static-pages/static-pages.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import 'lib/boot/routes'
 import router from 'lib/site/boot/router'
 import AcercaDe from './pages/acerca-de'
@@ -7,8 +8,27 @@ router.childRoutes.unshift({
   path: 's/acerca-de',
   component: AcercaDe
 })
+<<<<<<< HEAD
 
 router.childRoutes.unshift({
   path: 's/datos',
   component: Datos
 })
+=======
+=======
+import React from 'react'
+import {Route} from 'react-router'
+import 'lib/boot/routes'
+import router from 'lib/site/boot/router'
+import TerminosYCondiciones from './pages/terminos-y-condiciones'
+
+const site = router.find((route) => route.key === 'lib-site')
+
+site.props.children.unshift(
+  <Route
+    key='ext-terminos-y-condiciones'
+    path='/s/terminos-y-condiciones'
+    component={TerminosYCondiciones} />
+)
+>>>>>>> Agrega static-pages
+>>>>>>> Agrega static-pages
