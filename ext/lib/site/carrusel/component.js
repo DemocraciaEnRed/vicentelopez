bac053baecf3b393da522bb7d11965d58ab5c5f8
nigmata <fchiari@hotmail.com>
2017-07-07 14:58:57 -0300

    Arregla Carrusel Topic Cards

--- ext/lib/site/carrusel/component.js
+++ ext/lib/site/carrusel/component.js
@@ -29,31 +29,29 @@ export default class Carrusel extends Component{
       })
   }
 
-  carruselMount = (e) => {
-    console.log(e)
-    this.flkty = new Flickity(e, {
-      autoPlay: true,
-      wrapAround: true,
-      contain: true
-    })
-  }
+  componentDidUpdate (){
+    const carousel = this.refs.carousel;
+    const options = {
+        cellSelector: '.ext-topic-card',
+        contain: true,
+        initialIndex: 0,
+        accessibility: true
+    }
+
+    this.flkty = new Flickity(carousel, options);
+    this.flkty.on('cellSelect', this.updateSelected);
+
+}
+
+
 
   render(){
     const { forum, topics } = this.state
     return (
-      <div className='carrusel-seccion container-fluid'>
-        <div className='row'>
-          <div className='col-xs-10 offset-xs-1 col-md-8 offset-md-2 cont-barrio'>
-            <div className='titulo-verde'>
-              <h2>Proyectos</h2>
-            </div>
-          </div>
-        </div>
-        <div ref={this.carruselMount}>
-          {topics && topics.map((topic, i) => <TopicCard key={topic.id} forum={forum} topic={topic} />)}
-        </div>
-
+      <div ref='carousel' >
+        {topics && topics.map((topic, i) => <TopicCard key={topic.id} forum={forum} topic={topic} />)}
       </div>
+
 
     )
   }
