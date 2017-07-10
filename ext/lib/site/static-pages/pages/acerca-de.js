10cdc214274a81aedb697ccafd62e4420d0455f0
Valentin <valentinmuro@gmail.com>
2017-07-10 15:41:06 -0300

    Corrige maquetado en el Footer.

--- ext/lib/site/static-pages/pages/acerca-de.js
+++ ext/lib/site/static-pages/pages/acerca-de.js
@@ -97,23 +97,23 @@ export default function Page () {
           <div className="fila">
             <div className="cols col-1">
               <div>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum eget leo ut ullamcorper. Integer vitae erat nisl. Sed hendrerit egestas aliquet. Pellentesque maximus ullamcorper ligula, sit amet dignissim nunc rhoncus eget. Suspendisse imperdiet nisl quis dictum porta. Sed sit amet mi vitae felis bibendum pretium nec ac ligula. In dignissim at neque sed ullamcorper. Nunc massa mauris, congue vel felis quis, facilisis commodo mi. Ut bibendum nunc at eros interdum, sit amet iaculis magna aliquam.
-              </div>
-            </div>
-            <div className="cols col-2">
-              <div>
                 <div className="thumbs">
                   <div className="como-voto"></div>
                 </div>
               </div>
               <h2>¿Cómo voto?</h2>
             </div>
+            <div className="cols col-2">
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum eget leo ut ullamcorper. Integer vitae erat nisl. Sed hendrerit egestas aliquet. Pellentesque maximus ullamcorper ligula, sit amet dignissim nunc rhoncus eget. Suspendisse imperdiet nisl quis dictum porta. Sed sit amet mi vitae felis bibendum pretium nec ac ligula. In dignissim at neque sed ullamcorper. Nunc massa mauris, congue vel felis quis, facilisis commodo mi. Ut bibendum nunc at eros interdum, sit amet iaculis magna aliquam.
+              </div>
+            </div>
           </div>
           <div className="fila">
             <div className="cols col-1">
-            <div className="thumbs">
-              <div className="donde-voto"></div>
-            </div>
+              <div className="thumbs">
+                <div className="donde-voto"></div>
+              </div>
               <h2>¿Dónde voto?</h2>
             </div>
             <div className="cols col-2">
