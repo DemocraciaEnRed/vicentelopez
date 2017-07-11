99e6b390c9a2fcaa95d60ed1ca2c8ffc6aca1403
Valentin <valentinmuro@gmail.com>
2017-07-11 13:06:22 -0300

    Agrega página estática de Datos.

---
+++ ext/lib/site/static-pages/pages/datos.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import Footer from   'ext/lib/site/footer/component'
+import BannerForoVecinal from 'ext/lib/site/banner-foro-vecinal/component'
+import Carrusel from 'ext/lib/site/carrusel/component'
+
+export default function Page () {
+  return (
+    <div>
+      <BannerForoVecinal />
+      <div className='ext-terminos-y-condiciones container'>
+
+        <div className="filas">
+          <div className="fila">
+            <div className="cols col-1">
+              <div>
+                <div className="thumbs">
+                  <div className="que-son"></div>
+                </div>
+                <h2>¿Qué son?</h2>
+              </div>
+            </div>
+            <div className="cols col-2">
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum eget leo ut ullamcorper. Integer vitae erat nisl. Sed hendrerit egestas aliquet. Pellentesque maximus ullamcorper ligula, sit amet dignissim nunc rhoncus eget. Suspendisse imperdiet nisl quis dictum porta. Sed sit amet mi vitae felis bibendum pretium nec ac ligula. In dignissim at neque sed ullamcorper. Nunc massa mauris, congue vel felis quis, facilisis commodo mi. Ut bibendum nunc at eros interdum, sit amet iaculis magna aliquam.
+              </div>
+            </div>
+          </div>
+          <div className="fila">
+            <div className="cols col-1">
+              <div>
+                <div className="thumbs">
+                  <div className="como-voto"></div>
+                </div>
+              </div>
+              <h2>¿Cómo voto?</h2>
+            </div>
+            <div className="cols col-2">
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum eget leo ut ullamcorper. Integer vitae erat nisl. Sed hendrerit egestas aliquet. Pellentesque maximus ullamcorper ligula, sit amet dignissim nunc rhoncus eget. Suspendisse imperdiet nisl quis dictum porta. Sed sit amet mi vitae felis bibendum pretium nec ac ligula. In dignissim at neque sed ullamcorper. Nunc massa mauris, congue vel felis quis, facilisis commodo mi. Ut bibendum nunc at eros interdum, sit amet iaculis magna aliquam.
+              </div>
+            </div>
+          </div>
+          <div className="fila">
+            <div className="cols col-1">
+              <div className="thumbs">
+                <div className="donde-voto"></div>
+              </div>
+              <h2>¿Dónde voto?</h2>
+            </div>
+            <div className="cols col-2">
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum eget leo ut ullamcorper. Integer vitae erat nisl. Sed hendrerit egestas aliquet. Pellentesque maximus ullamcorper ligula, sit amet dignissim nunc rhoncus eget. Suspendisse imperdiet nisl quis dictum porta. Sed sit amet mi vitae felis bibendum pretium nec ac ligula. In dignissim at neque sed ullamcorper. Nunc massa mauris, congue vel felis quis, facilisis commodo mi. Ut bibendum nunc at eros interdum, sit amet iaculis magna aliquam.
+              </div>
+            </div>
+          </div>
+          <div className="fila no-bg">
+            <div className="map-box">
+              <div className='mapa'></div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+          <Carrusel />
+          <Footer />
+    </div>
+  )
+}
--- ext/lib/site/static-pages/static-pages.js
+++ ext/lib/site/static-pages/static-pages.js
@@ -1,8 +1,9 @@
 import 'lib/boot/routes'
 import router from 'lib/site/boot/router'
 import AcercaDe from './pages/acerca-de'
+import Datos from './pages/datos'
 
 router.childRoutes.unshift({
-  path: 's/acerca-de',
-  component: AcercaDe
+  path: 's/datos',
+  component: Datos
 })
