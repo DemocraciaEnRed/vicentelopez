a71c14ea1448cd54f9e522e807ae0d68ab746af7
Valentin <valentinmuro@gmail.com>
2017-07-11 15:20:03 -0300

    Agrega un montón de gráficos.

--- ext/lib/site/static-pages/pages/datos.js
+++ ext/lib/site/static-pages/pages/datos.js
@@ -9,57 +9,37 @@ export default function Page () {
       <BannerForoVecinal title="Los foros en datos"/>
       <div className='ext-datos container'>
 
-        <div className="filas">
-          <div className="fila">
-            <div className="cols col-1">
-              <div>
-                <div className="thumbs">
-                  <div className="que-son"></div>
-                </div>
-                <h2>¿Qué son?</h2>
-              </div>
-            </div>
-            <div className="cols col-2">
-              <div>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum eget leo ut ullamcorper. Integer vitae erat nisl. Sed hendrerit egestas aliquet. Pellentesque maximus ullamcorper ligula, sit amet dignissim nunc rhoncus eget. Suspendisse imperdiet nisl quis dictum porta. Sed sit amet mi vitae felis bibendum pretium nec ac ligula. In dignissim at neque sed ullamcorper. Nunc massa mauris, congue vel felis quis, facilisis commodo mi. Ut bibendum nunc at eros interdum, sit amet iaculis magna aliquam.
-              </div>
-            </div>
-          </div>
-          <div className="fila">
-            <div className="cols col-1">
-              <div>
-                <div className="thumbs">
-                  <div className="como-voto"></div>
-                </div>
-              </div>
-              <h2>¿Cómo voto?</h2>
-            </div>
-            <div className="cols col-2">
-              <div>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum eget leo ut ullamcorper. Integer vitae erat nisl. Sed hendrerit egestas aliquet. Pellentesque maximus ullamcorper ligula, sit amet dignissim nunc rhoncus eget. Suspendisse imperdiet nisl quis dictum porta. Sed sit amet mi vitae felis bibendum pretium nec ac ligula. In dignissim at neque sed ullamcorper. Nunc massa mauris, congue vel felis quis, facilisis commodo mi. Ut bibendum nunc at eros interdum, sit amet iaculis magna aliquam.
-              </div>
-            </div>
-          </div>
-          <div className="fila">
-            <div className="cols col-1">
-              <div className="thumbs">
-                <div className="donde-voto"></div>
-              </div>
-              <h2>¿Dónde voto?</h2>
-            </div>
-            <div className="cols col-2">
-              <div>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum eget leo ut ullamcorper. Integer vitae erat nisl. Sed hendrerit egestas aliquet. Pellentesque maximus ullamcorper ligula, sit amet dignissim nunc rhoncus eget. Suspendisse imperdiet nisl quis dictum porta. Sed sit amet mi vitae felis bibendum pretium nec ac ligula. In dignissim at neque sed ullamcorper. Nunc massa mauris, congue vel felis quis, facilisis commodo mi. Ut bibendum nunc at eros interdum, sit amet iaculis magna aliquam.
-              </div>
-            </div>
-          </div>
-          <div className="fila no-bg">
-            <div className="map-box">
-              <div className='mapa'></div>
-            </div>
+        <section className="intro">
 
+          <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas aliquet luctus. Curabitur vestibulum lorem quis porta interdum. Fusce ullamcorper lectus tortor, nec sagittis odio fringilla ac. Curabitur rutrum libero ac urna pretium, in bibendum neque tincidunt. Suspendisse vitae elementum sapien. Donec et libero ut ipsum vestibulum imperdiet. Mauris commodo orci nisi, id ultricies magna blandit at. Curabitur in lectus eu enim tincidunt dignissim quis vitae velit. Donec vehicula risus arcu. Etiam laoreet nibh lacinia rutrum euismod. Vivamus eget velit magna. Proin faucibus leo nec volutpat ullamcorper. In hac habitasse platea dictumst. Duis quis sem efficitur, semper leo at, gravida nunc. Sed condimentum non mi ut porta.</div>
+        </section>
+        <section className="progreso-proyectos">
+          <h3>Proyectos por año</h3>
+          <div className="graph">
+            <img src="ext/lib/site/static-pages/progreso-proyectos.svg" alt=""/>
           </div>
-        </div>
+          <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas aliquet luctus. Curabitur vestibulum lorem quis porta interdum. Fusce ullamcorper lectus tortor, nec sagittis odio fringilla ac. Curabitur rutrum libero ac urna pretium, in bibendum neque tincidunt. Suspendisse vitae elementum sapien. Donec et libero ut ipsum vestibulum imperdiet. Mauris commodo orci nisi, id ultricies magna blandit at. Curabitur in lectus eu enim tincidunt dignissim quis vitae velit. Donec vehicula risus arcu. Etiam laoreet nibh lacinia rutrum euismod. Vivamus eget velit magna. Proin faucibus leo nec volutpat ullamcorper. In hac habitasse platea dictumst. Duis quis sem efficitur, semper leo at, gravida nunc. Sed condimentum non mi ut porta.</div>
+        </section>
+        <section className="progreso-proyectos-por-tipo">
+          <h3>Cantidad de proyectos por año por tipo</h3>
+          <div className="graph">
+          </div>
+          <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas aliquet luctus. Curabitur vestibulum lorem quis porta interdum. Fusce ullamcorper lectus tortor, nec sagittis odio fringilla ac. Curabitur rutrum libero ac urna pretium, in bibendum neque tincidunt. Suspendisse vitae elementum sapien. Donec et libero ut ipsum vestibulum imperdiet. Mauris commodo orci nisi, id ultricies magna blandit at. Curabitur in lectus eu enim tincidunt dignissim quis vitae velit. Donec vehicula risus arcu. Etiam laoreet nibh lacinia rutrum euismod. Vivamus eget velit magna. Proin faucibus leo nec volutpat ullamcorper. In hac habitasse platea dictumst. Duis quis sem efficitur, semper leo at, gravida nunc. Sed condimentum non mi ut porta.</div>
+        </section>
+        <section className="distribucion-proyectos">
+          <h3>Distribución de proyectos por tipo</h3>
+          <div className="graph">
+          </div>
+          <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas aliquet luctus. Curabitur vestibulum lorem quis porta interdum. Fusce ullamcorper lectus tortor, nec sagittis odio fringilla ac. Curabitur rutrum libero ac urna pretium, in bibendum neque tincidunt. Suspendisse vitae elementum sapien. Donec et libero ut ipsum vestibulum imperdiet. Mauris commodo orci nisi, id ultricies magna blandit at. Curabitur in lectus eu enim tincidunt dignissim quis vitae velit. Donec vehicula risus arcu. Etiam laoreet nibh lacinia rutrum euismod. Vivamus eget velit magna. Proin faucibus leo nec volutpat ullamcorper. In hac habitasse platea dictumst. Duis quis sem efficitur, semper leo at, gravida nunc. Sed condimentum non mi ut porta.</div>
+        </section>
+        <section className="evolucion-participacion">
+          <h3>Evolución de la participación</h3>
+          <div className="graph">
+          </div>
+          <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas aliquet luctus. Curabitur vestibulum lorem quis porta interdum. Fusce ullamcorper lectus tortor, nec sagittis odio fringilla ac. Curabitur rutrum libero ac urna pretium, in bibendum neque tincidunt. Suspendisse vitae elementum sapien. Donec et libero ut ipsum vestibulum imperdiet. Mauris commodo orci nisi, id ultricies magna blandit at. Curabitur in lectus eu enim tincidunt dignissim quis vitae velit. Donec vehicula risus arcu. Etiam laoreet nibh lacinia rutrum euismod. Vivamus eget velit magna. Proin faucibus leo nec volutpat ullamcorper. In hac habitasse platea dictumst. Duis quis sem efficitur, semper leo at, gravida nunc. Sed condimentum non mi ut porta.</div>
+        </section>
+
+
       </div>
           <Carrusel />
           <Footer />
