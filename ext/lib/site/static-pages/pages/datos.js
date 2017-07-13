0b41b239f4a49b5dd8765c26d66856aa3b9e1917
Valentin <valentinmuro@gmail.com>
2017-07-13 14:21:00 -0300

    Corrige rebase.

--- ext/lib/site/static-pages/pages/datos.js
+++ ext/lib/site/static-pages/pages/datos.js
@@ -2,37 +2,15 @@ import React from 'react'
 import Footer from   'ext/lib/site/footer/component'
 import BannerForoVecinal from 'ext/lib/site/banner-foro-vecinal/component'
 import Carrusel from 'ext/lib/site/carrusel/component'
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import CarruselAnios from 'ext/lib/site/carrusel-anios/component'
-=======
->>>>>>> Agrega página estática de Datos.
-=======
-import CarruselAnios from 'ext/lib/site/carrusel-anios/component'
->>>>>>> Agrega Carousel de Datos.
-=======
-import CarruselAnios from 'ext/lib/site/carrusel-anios/component'
->>>>>>> a0ad60d163428e8ab7a405c97c45d5fb01e995ac
 
 export default function Page () {
   return (
     <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> a0ad60d163428e8ab7a405c97c45d5fb01e995ac
       <BannerForoVecinal title="Los foros en datos"/>
       <div className='ext-datos container'>
 
         <section className="intro">
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> a0ad60d163428e8ab7a405c97c45d5fb01e995ac
           <div className="text=box">
             <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas aliquet luctus. Curabitur vestibulum lorem quis porta interdum. Fusce ullamcorper lectus tortor, nec sagittis odio fringilla ac. Curabitur rutrum libero ac urna pretium, in bibendum neque tincidunt. Suspendisse vitae elementum sapien. Donec et libero ut ipsum vestibulum imperdiet. Mauris commodo orci nisi, id ultricies magna blandit at. Curabitur in lectus eu enim tincidunt dignissim quis vitae velit. Donec vehicula risus arcu. Etiam laoreet nibh lacinia rutrum euismod. Vivamus eget velit magna. Proin faucibus leo nec volutpat ullamcorper. In hac habitasse platea dictumst. Duis quis sem efficitur, semper leo at, gravida nunc. Sed condimentum non mi ut porta.</div>
           </div>
@@ -62,10 +40,6 @@ export default function Page () {
           <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas aliquet luctus. Curabitur vestibulum lorem quis porta interdum. Fusce ullamcorper lectus tortor, nec sagittis odio fringilla ac. Curabitur rutrum libero ac urna pretium, in bibendum neque tincidunt. Suspendisse vitae elementum sapien. Donec et libero ut ipsum vestibulum imperdiet. Mauris commodo orci nisi, id ultricies magna blandit at. Curabitur in lectus eu enim tincidunt dignissim quis vitae velit. Donec vehicula risus arcu. Etiam laoreet nibh lacinia rutrum euismod. Vivamus eget velit magna. Proin faucibus leo nec volutpat ullamcorper. In hac habitasse platea dictumst. Duis quis sem efficitur, semper leo at, gravida nunc. Sed condimentum non mi ut porta.</div>
           </div>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> a0ad60d163428e8ab7a405c97c45d5fb01e995ac
           </div>
 
         </section>
@@ -101,127 +75,6 @@ export default function Page () {
         </section>
 
 
-<<<<<<< HEAD
-=======
-      <BannerForoVecinal />
-=======
-      <BannerForoVecinal title="Los foros en datos"/>
->>>>>>> Corrige acerca de y datos.
-=======
-      <BannerForoVecinal title="Los foros en datos"/>
->>>>>>> Corrige conflictos de acerca de y datos
-      <div className='ext-datos container'>
-
-        <div className="filas">
-          <div className="fila">
-            <div className="cols col-1">
-              <div>
-                <div className="thumbs">
-                  <div className="que-son"></div>
-                </div>
-                <h2>¿Qué son?</h2>
-              </div>
-            </div>
-            <div className="cols col-2">
-              <div>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum eget leo ut ullamcorper. Integer vitae erat nisl. Sed hendrerit egestas aliquet. Pellentesque maximus ullamcorper ligula, sit amet dignissim nunc rhoncus eget. Suspendisse imperdiet nisl quis dictum porta. Sed sit amet mi vitae felis bibendum pretium nec ac ligula. In dignissim at neque sed ullamcorper. Nunc massa mauris, congue vel felis quis, facilisis commodo mi. Ut bibendum nunc at eros interdum, sit amet iaculis magna aliquam.
-              </div>
-            </div>
-=======
-
-          <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas aliquet luctus. Curabitur vestibulum lorem quis porta interdum. Fusce ullamcorper lectus tortor, nec sagittis odio fringilla ac. Curabitur rutrum libero ac urna pretium, in bibendum neque tincidunt. Suspendisse vitae elementum sapien. Donec et libero ut ipsum vestibulum imperdiet. Mauris commodo orci nisi, id ultricies magna blandit at. Curabitur in lectus eu enim tincidunt dignissim quis vitae velit. Donec vehicula risus arcu. Etiam laoreet nibh lacinia rutrum euismod. Vivamus eget velit magna. Proin faucibus leo nec volutpat ullamcorper. In hac habitasse platea dictumst. Duis quis sem efficitur, semper leo at, gravida nunc. Sed condimentum non mi ut porta.</div>
-        </section>
-        <section className="progreso-proyectos">
-          <h3>Proyectos por año</h3>
-<<<<<<< HEAD
-          <div className="graph">
-            <img src="ext/lib/site/static-pages/progreso-proyectos.svg" alt=""/>
->>>>>>> Agrega un montón de gráficos.
-=======
-          <div className="graph-box">
-            <img className="graph" src="../ext/lib/site/static-pages/progreso-proyectos.svg" alt=""/>
-            <img className="graph-ref" src="../ext/lib/site/static-pages/progreso-proyectos-ref.svg" alt=""/>
->>>>>>> Agrega gráficos.
-          </div>
-          <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas aliquet luctus. Curabitur vestibulum lorem quis porta interdum. Fusce ullamcorper lectus tortor, nec sagittis odio fringilla ac. Curabitur rutrum libero ac urna pretium, in bibendum neque tincidunt. Suspendisse vitae elementum sapien. Donec et libero ut ipsum vestibulum imperdiet. Mauris commodo orci nisi, id ultricies magna blandit at. Curabitur in lectus eu enim tincidunt dignissim quis vitae velit. Donec vehicula risus arcu. Etiam laoreet nibh lacinia rutrum euismod. Vivamus eget velit magna. Proin faucibus leo nec volutpat ullamcorper. In hac habitasse platea dictumst. Duis quis sem efficitur, semper leo at, gravida nunc. Sed condimentum non mi ut porta.</div>
-=======
-          <div className="text=box">
-            <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas aliquet luctus. Curabitur vestibulum lorem quis porta interdum. Fusce ullamcorper lectus tortor, nec sagittis odio fringilla ac. Curabitur rutrum libero ac urna pretium, in bibendum neque tincidunt. Suspendisse vitae elementum sapien. Donec et libero ut ipsum vestibulum imperdiet. Mauris commodo orci nisi, id ultricies magna blandit at. Curabitur in lectus eu enim tincidunt dignissim quis vitae velit. Donec vehicula risus arcu. Etiam laoreet nibh lacinia rutrum euismod. Vivamus eget velit magna. Proin faucibus leo nec volutpat ullamcorper. In hac habitasse platea dictumst. Duis quis sem efficitur, semper leo at, gravida nunc. Sed condimentum non mi ut porta.</div>
-          </div>
-        </section>
-        <section className="progreso-proyectos">
-          <h3>Proyectos por año</h3>
-          <div className="flexBox">
-            <div className="graph-box">
-              <img className="graph" src="../ext/lib/site/static-pages/progreso-proyectos.svg" alt=""/>
-              <img className="graph-ref" src="../ext/lib/site/static-pages/progreso-proyectos-ref.svg" alt=""/>
-            </div>
-            <div className="text-box">
-              <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas aliquet luctus. Curabitur vestibulum lorem quis porta interdum. Fusce ullamcorper lectus tortor, nec sagittis odio fringilla ac. Curabitur rutrum libero ac urna pretium, in bibendum neque tincidunt. Suspendisse vitae elementum sapien. Donec et libero ut ipsum vestibulum imperdiet. Mauris commodo orci nisi, id ultricies magna blandit at. Curabitur in lectus eu enim tincidunt dignissim quis vitae velit. Donec vehicula risus arcu. Etiam laoreet nibh lacinia rutrum euismod. Vivamus eget velit magna. Proin faucibus leo nec volutpat ullamcorper. In hac habitasse platea dictumst. Duis quis sem efficitur, semper leo at, gravida nunc. Sed condimentum non mi ut porta.</div>
-            </div>
-        </div>
->>>>>>> Arregla Datos. Arregla Banner.
-        </section>
-        <section className="progreso-proyectos-por-tipo">
-          <h3>Cantidad de proyectos por año por tipo</h3>
-          <div className="flexBox">
-
-          <div className="graph-box">
-            <img className="graph" src="../ext/lib/site/static-pages/progreso-proyectos-por-tipo.svg" alt=""/>
-            <img className="graph-ref" src="../ext/lib/site/static-pages/progreso-proyectos-por-tipo-ref.svg" alt=""/>
-          </div>
-          <div className="text-box">
-
-          <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas aliquet luctus. Curabitur vestibulum lorem quis porta interdum. Fusce ullamcorper lectus tortor, nec sagittis odio fringilla ac. Curabitur rutrum libero ac urna pretium, in bibendum neque tincidunt. Suspendisse vitae elementum sapien. Donec et libero ut ipsum vestibulum imperdiet. Mauris commodo orci nisi, id ultricies magna blandit at. Curabitur in lectus eu enim tincidunt dignissim quis vitae velit. Donec vehicula risus arcu. Etiam laoreet nibh lacinia rutrum euismod. Vivamus eget velit magna. Proin faucibus leo nec volutpat ullamcorper. In hac habitasse platea dictumst. Duis quis sem efficitur, semper leo at, gravida nunc. Sed condimentum non mi ut porta.</div>
-          </div>
-          
-=======
->>>>>>> Agrega Carousel de Datos.
-          </div>
-
-        </section>
-        <section className="distribucion-proyectos">
-          <h3>Distribución de proyectos por tipo</h3>
-          <div className="flexBox">
-
-          <div className="graph-box">
-          <CarruselAnios />
-          </div>
-          <div className="text-box">
-
-          <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas aliquet luctus. Curabitur vestibulum lorem quis porta interdum. Fusce ullamcorper lectus tortor, nec sagittis odio fringilla ac. Curabitur rutrum libero ac urna pretium, in bibendum neque tincidunt. Suspendisse vitae elementum sapien. Donec et libero ut ipsum vestibulum imperdiet. Mauris commodo orci nisi, id ultricies magna blandit at. Curabitur in lectus eu enim tincidunt dignissim quis vitae velit. Donec vehicula risus arcu. Etiam laoreet nibh lacinia rutrum euismod. Vivamus eget velit magna. Proin faucibus leo nec volutpat ullamcorper. In hac habitasse platea dictumst. Duis quis sem efficitur, semper leo at, gravida nunc. Sed condimentum non mi ut porta.</div>
-          </div>
-
-          </div>
-
-        </section>
-        <section className="evolucion-participacion">
-          <h3>Evolución de la participación</h3>
-          <div className="flexBox">
-
-          <div className="graph-box">
-            <img className="graph" src="../ext/lib/site/static-pages/evolucion-participacion.svg" alt=""/>
-          </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-        </div>
->>>>>>> Agrega página estática de Datos.
-=======
-=======
-          <div className="text-box">
-
->>>>>>> Arregla Datos. Arregla Banner.
-          <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas aliquet luctus. Curabitur vestibulum lorem quis porta interdum. Fusce ullamcorper lectus tortor, nec sagittis odio fringilla ac. Curabitur rutrum libero ac urna pretium, in bibendum neque tincidunt. Suspendisse vitae elementum sapien. Donec et libero ut ipsum vestibulum imperdiet. Mauris commodo orci nisi, id ultricies magna blandit at. Curabitur in lectus eu enim tincidunt dignissim quis vitae velit. Donec vehicula risus arcu. Etiam laoreet nibh lacinia rutrum euismod. Vivamus eget velit magna. Proin faucibus leo nec volutpat ullamcorper. In hac habitasse platea dictumst. Duis quis sem efficitur, semper leo at, gravida nunc. Sed condimentum non mi ut porta.</div>
-          </div>
-
-          </div>
-
-        </section>
-
-
->>>>>>> Agrega un montón de gráficos.
-=======
->>>>>>> a0ad60d163428e8ab7a405c97c45d5fb01e995ac
       </div>
           <Carrusel />
           <Footer />
--- ext/lib/site/static-pages/static-pages.js
+++ ext/lib/site/static-pages/static-pages.js
@@ -4,65 +4,11 @@ import AcercaDe from './pages/acerca-de'
 import Datos from './pages/datos'
 
 router.childRoutes.unshift({
-  path: 's/datos',
-  component: Datos
-})
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-router.childRoutes.unshift({
-  path: 's/datos',
-  component: Datos
-})
-=======
-=======
->>>>>>> Corrige Acerca De (clases CSS) y Datos.
-=======
-import React from 'react'
-import {Route} from 'react-router'
-=======
->>>>>>> Corrige static pages
-import 'lib/boot/routes'
-import router from 'lib/site/boot/router'
-import AcercaDe from './pages/acerca-de'
-
-<<<<<<< HEAD
-const site = router.find((route) => route.key === 'lib-site')
-
-site.props.children.unshift(
-  <Route
-    key='ext-terminos-y-condiciones'
-    path='/s/terminos-y-condiciones'
-    component={TerminosYCondiciones} />
-)
->>>>>>> Agrega static-pages
-<<<<<<< HEAD
->>>>>>> Agrega static-pages
-=======
-=======
-=======
-=======
->>>>>>> Corrige acerca de y datos.
-=======
->>>>>>> a0ad60d163428e8ab7a405c97c45d5fb01e995ac
-
-router.childRoutes.unshift({
   path: 's/acerca-de',
   component: AcercaDe
 })
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> Corrige static pages
-<<<<<<< HEAD
->>>>>>> Corrige static pages
-=======
-=======
->>>>>>> Corrige Acerca De (clases CSS) y Datos.
->>>>>>> Corrige Acerca De (clases CSS) y Datos.
-=======
->>>>>>> Corrige acerca de y datos.
-=======
->>>>>>> a0ad60d163428e8ab7a405c97c45d5fb01e995ac
+
+router.childRoutes.unshift({
+  path: 's/datos',
+  component: Datos
+})
