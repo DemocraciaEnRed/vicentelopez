99f8e8ffa28bd013ac1d4bb69d1ed58e077e4c43
Valentin <valentinmuro@gmail.com>
2017-07-26 12:46:39 -0300

    Corrige presentación de datos en portada.

--- ext/lib/site/foros-en-datos/component.js
+++ ext/lib/site/foros-en-datos/component.js
@@ -11,7 +11,10 @@ export default function ForosEnDatos () {
         <h3>Distribución de proyectos por tipo</h3>
         <div className="flexBox">
           <div className="graph-box">
-          <CarruselAnios />
+            <CarruselAnios />
+          </div>
+          <div className="text-box">
+            <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas aliquet luctus. Curabitur vestibulum lorem quis porta interdum. Fusce ullamcorper lectus tortor, nec sagittis odio fringilla ac. Curabitur rutrum libero ac urna pretium, in bibendum neque tincidunt. Suspendisse vitae elementum sapien. Donec et libero ut ipsum vestibulum imperdiet. Mauris commodo orci nisi, id ultricies magna blandit at. Curabitur in lectus eu enim tincidunt dignissim quis vitae velit. Donec vehicula risus arcu. Etiam laoreet nibh lacinia rutrum euismod. Vivamus eget velit magna. Proin faucibus leo nec volutpat ullamcorper. In hac habitasse platea dictumst. Duis quis sem efficitur, semper leo at, gravida nunc. Sed condimentum non mi ut porta.</div>
           </div>
         </div>
         <div className='cont-boton-azul'>
