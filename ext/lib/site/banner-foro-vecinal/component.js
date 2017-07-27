20118bba34702facdc0d310a56808bb4e2310012
Fran <francisco@democracyos.io>
2017-07-27 14:03:15 -0300

    agregar videos home

--- ext/lib/site/banner-foro-vecinal/component.js
+++ ext/lib/site/banner-foro-vecinal/component.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
-
-
-
 export default function BannerForoVecinal (props) {
-
   return (
     <section className="container-fluid banner-fv ">
       <div className="banner">
-
+        <video playsInline autoPlay muted loop>
+          <source src="ext/lib/site/banner-foro-vecinal/banner.webm" type="video/webm" />
+          <source src="ext/lib/site/banner-foro-vecinal/banner.mp4" type="video/mp4" />
+        </video>
       </div>
       <div
         className='contenedor'>
