57cf84517bf07db732f0cf982dbadc7829be65c1
Francisco P <francisco@democracyos.io>
2017-08-16 17:11:53 -0300

    maquetando datos por foro

--- ext/lib/site/datos-forum/component.js
+++ ext/lib/site/datos-forum/component.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import checkReservedNames from 'lib/forum/check-reserved-names'
 import forumStore from 'lib/stores/forum-store/forum-store'
+import {Link} from 'react-router'
 
 export default function DatosPorForo (props) {
   //console.log(props.forum)
@@ -11,9 +11,8 @@ export default function DatosPorForo (props) {
     <section className='seccion-datos-por-foro'>
       <div className='datos-wrapper'>
         <div className='recuadro-datos-foro'>
-          <h2>Datos del Barrio</h2>
-          <p>Primer dato</p>
-          <p>Segundo dato</p>
+          <p>El presupuesto asignado a [Barrio] es de [Presupuesto]</p>
+          <a className="boton-azul btn" href="#">DESCARGAR BOLETA</a>
         </div>
       </div>
     </section>
