f82fa702f5cd546e2fcde393248ee5d0de7745ff
trixtar <rita@democracyos.io>
2017-08-28 14:52:07 -0300

    tabla datos y pagina responsiva

--- ext/lib/site/static-pages/pages/datos.js
+++ ext/lib/site/static-pages/pages/datos.js
@@ -20,49 +20,126 @@ export default class Page extends Component {
       <div>
         <BannerForoVecinal title="Los foros en datos"/>
         <Anchor id='container'>
+
           <div className='ext-datos'>
-            <div className="text">
+            <p className="text">
               El presupuesto participativo de Vicente López se viene haciendo de manera ininterrumpida desde el 2012. Aquí encontrarás información y datos históricos de todas las ediciones de Foros Vecinales.
-            </div>
-            <h3>Proyectos por año</h3>
-            <div className="seccion-datos">
-              <div className="graph-box">
-                <img className="graph" src="/ext/lib/site/static-pages/progreso-proyectos.svg" alt=""/>
-              </div>
-              <div className="text">
-                Evolución de cantidad de proyectos presentados, factibles y ganadores año a año.
-                <img className="graph-ref" src="/ext/lib/site/static-pages/progreso-proyectos-ref.svg" alt=""/>
-              </div>
-            </div>
+            </p>
+            
+            <article className="seccion-datos">
+              <h3>Cantidad de proyectos por año</h3>
+              <figure className="graph-box">
+                <div className="graph-img">
+                  <img src="/ext/lib/site/static-pages/progreso-proyectos.svg" alt=""/>
+                </div>
+                <figcaption>
+                  <p>Evolución de cantidad de proyectos presentados, aprobados y ejecutados año a año.</p>
+                  <img className="graph-ref" src="/ext/lib/site/static-pages/progreso-proyectos-ref.png" alt=""/>
+                </figcaption>
+              </figure>
+            </article>
+
+            <article className="seccion-datos">
             <h3>Cantidad de proyectos por año por categoría</h3>
-            <div className="seccion-datos">
-              <div className="graph-box">
-                <img className="graph" src="/ext/lib/site/static-pages/progreso-proyectos-por-tipo.svg" alt=""/>
-              </div>
-              <div className="text">
-                Evolución en el tiempo de cantidad de proyectos por categoría (cultura, deporte, educación, fiscalizacion, gobierno, planeamiento, salud, seguridad y tránsito).
-                <img className="graph-ref" src="/ext/lib/site/static-pages/progreso-proyectos-por-tipo-ref.svg" alt=""/>
-              </div>
-            </div>
+              <figure className="graph-box">
+                <div className="graph-img">
+                  <img src="/ext/lib/site/static-pages/progreso-proyectos-por-tipo.svg" alt=""/>
+                </div>
+                <figcaption>
+                  <p>Evolución en el tiempo de cantidad de proyectos por categoría (cultura, deporte, educación, fiscalizacion, gobierno, planeamiento, salud, seguridad y tránsito).</p>
+                  <img className="graph-ref" src="/ext/lib/site/static-pages/progreso-proyectos-por-tipo-ref.svg" alt=""/>
+                </figcaption>
+              </figure>
+            </article>
 
-            <h3>Distribución de proyectos por categoría</h3>
-            <div className="graph-box">
-              <CarruselAnios />
-            </div>
-            <div className="text">
-              Visualización por año de la distribución de proyectos por categoría (cultura, deporte, educación, fiscalizacion, gobierno, planeamiento, salud, seguridad y tránsito).
-            </div>
+            <article className="seccion-datos">
+              <h3>Distribución de proyectos por categoría</h3>
+              <figure className="graph-box">
+                <CarruselAnios />
+                <figcaption>
+                  <p>Visualización por año de la distribución de proyectos por categoría (cultura, deporte, educación, fiscalizacion, gobierno, planeamiento, salud, seguridad y tránsito).</p>
+                </figcaption>
+              </figure>
+            </article>
 
+            <article className="seccion-datos">
+              <h3>Evolución de la participación</h3>
+              <figure className="graph-box">
+                <div className="graph-img">
+                  <img src="/ext/lib/site/static-pages/evolucion-participacion.svg" alt=""/>
+                </div>
+                <figcaption>
+                  <p>Evolución de ciudadanos (porcentaje de la población de Vicente López) que han participado en los foros vecinales de Vicente López.</p>
+                </figcaption>
+              </figure>
+            </article>
 
-            <h3>Evolución de la participación</h3>
-            <div className="seccion-datos">
-              <div className="graph-box">
-                <img className="graph" src="/ext/lib/site/static-pages/evolucion-participacion.svg" alt=""/>
-              </div>
-              <div className="text">
-                Evolución de ciudadanos (porcentaje de la población de Vicente López) que han participado en los foros vecinales de Vicente López.
-              </div>
-            </div>
+            <article className="seccion-datos">
+              <h3>Evolución del Presupuesto Participativo 2012 - 2017</h3>
+              <figure className="graph-box">
+                <table className="tabla-presupuestoparticipativo">
+                  <tbody>
+                    <tr>
+                      <td>Edición</td>
+                      <td>2012 - 2013</td>
+                      <td>2013 - 2014</td>
+                      <td>2014 - 2015</td>
+                      <td>2015 - 2016</td>
+                      <td>2016 - 2017</td>
+                    </tr>
+                    <tr>
+                      <td>Presupuesto Asignado</td>
+                      <td>10 MM</td>
+                      <td>15 MM</td>
+                      <td>20 MM</td>
+                      <td>30 MM</td>
+                      <td>45 MM</td>
+                    </tr>
+                    <tr>
+                      <td>Propuestas Presentadas</td>
+                      <td>86</td>
+                      <td>165</td>
+                      <td>277</td>
+                      <td>465</td>
+                      <td>348</td>
+                    </tr>
+                    <tr>
+                      <td>Proyectos Factibles</td>
+                      <td>65</td>
+                      <td>104</td>
+                      <td>153</td>
+                      <td>152</td>
+                      <td>148</td>
+                    </tr>
+                    <tr>
+                      <td>Proyectos Ganadores</td>
+                      <td>50</td>
+                      <td>69</td>
+                      <td>69</td>
+                      <td>63</td>
+                      <td>81</td>
+                    </tr>
+                    <tr>
+                      <td>Cantidad de Participantes</td>
+                      <td>1.178</td>
+                      <td>6.691</td>
+                      <td>10.068</td>
+                      <td>18.880</td>
+                      <td>26.537</td>
+                    </tr>
+                    <tr>
+                      <td>Proporción de Participantes</td>
+                      <td>1 cada 156</td>
+                      <td>1 cada 31</td>
+                      <td>1 cada 20</td>
+                      <td>1 cada 11</td>
+                      <td>1 cada 8</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </figure>
+            </article>
+
             <div className='descargas'>
               <h4>DESCARGAS</h4>
               <div className="btns-descargas">
