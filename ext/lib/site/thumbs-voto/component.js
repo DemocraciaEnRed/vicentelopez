9a568323fd5d16785973285649e222f6f8686303
trixtar <rita@democracyos.io>
2017-08-29 14:39:31 -0300

    link donde voto en home

--- ext/lib/site/thumbs-voto/component.js
+++ ext/lib/site/thumbs-voto/component.js
@@ -48,10 +48,11 @@ export default function ThumbsVoto (props) {
               ¿Dónde Voto?
             </h2>
             <p className="donde-voto-cont">
-            Encontrarás más de 100 urnas habilitadas para votar en los 9 barrios de Vicente López. La votación estará abierta desde el lunes 18 de septiembre hasta el domingo 1 de octubre de 2017, de lunes a viernes entre las 8:30hs a 16hs. 
-            <a href='/s/acerca-de#mapa'> Consultá todos los puntos de votación aquí.</a>
+            Encontrarás más de 100 urnas habilitadas para votar en los 9 barrios de Vicente López. La votación estará abierta desde el lunes 18 de septiembre hasta el domingo 1 de octubre de 2017, de lunes a viernes entre las 8:30hs a 16hs.
+              <Link to='/s/acerca-de#mapa'>
+                Consultá todos los puntos de votación aquí.
+              </Link>
             </p>
-
           </div>
 
 
