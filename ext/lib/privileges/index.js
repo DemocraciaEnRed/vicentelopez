94c3cc671f03cbae13cfefdec9103197d427f944
Matías Lescano <mjlescano@protonmail.com>
2018-03-03 12:04:59 -0300

    Agregar restriccion para comentar anteproyectos rechazados

--- ext/lib/privileges/index.js
+++ ext/lib/privileges/index.js
@@ -1,6 +1,8 @@
 const topicPrivileges = require('lib/privileges/topic')
 const { isCitizenOnProposal } = require('../proposals')
 
+// Not allow to edit or delete a Proposal by its author
+// when the state is "rejected" or "feasible" (not "pending")
 ;[
   'canEdit',
   'canDelete'
@@ -15,3 +17,17 @@ const { isCitizenOnProposal } = require('../proposals')
     return original(forum, user, topic)
   }
 })
+
+// Not allow to create new comments by citizens
+// when the Proposal state is "rejected"
+;(() => {
+  const original = topicPrivileges.canVote
+
+  topicPrivileges.canVote = (forum, user, topic) => {
+    if (isCitizenOnProposal(user, forum)) {
+      if (topic.attrs.state === 'rejected') return false
+    }
+
+    return original(forum, user, topic)
+  }
+})()
