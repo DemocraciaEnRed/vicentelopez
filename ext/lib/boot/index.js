e74cc654389246b4c49bfc2740bae17cb71964e3
franciclo <francisco@democracyos.io>
2018-03-04 21:34:50 -0300

    remove privileges override for now

--- ext/lib/boot/index.js
+++ ext/lib/boot/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = module.exports = express()
 
 require('ext/lib/notifier')
-require('ext/lib/privileges')
+// require('ext/lib/privileges')
 
 app.use('/ext/api', require('ext/lib/api'))
 app.use('/api/v2', require('ext/lib/api-v2'))
