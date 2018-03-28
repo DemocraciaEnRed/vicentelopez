ea08cb3ba536ddcf4e02d7002fee36a320948070
luarar <laurarh.971@gmail.com>
2018-03-28 10:40:59 -0300

    add buttons and update card

--- ext/lib/site/static-pages/pages/documentos.js
+++ ext/lib/site/static-pages/pages/documentos.js
@@ -2,47 +2,85 @@ import React, { Component } from 'react'
 import Footer from 'ext/lib/site/footer/component'
 
 export default class Page extends Component {
-  render () {
-    return (
-      <div>
-        <section className='banner-static'>
-          <div className='banner gradient'>
-            <h1>Título de la sección</h1>
-            <h2>Descripción del contenido dentro de la sección</h2>
-          </div>
-        </section>
-        <div className='documentos-body'>
-          <div className='select-barrio'>
-            <p>Seleccioná el barrio que quieras visualizar</p>
+  constructor (props) {
+    super(props)
+    this.state = {
+      anioDeBoton: '2012',
+      barrio: 'Villa Marteli'
+    // coloca un estado inicial del mensaje
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+  };
 
-            <div className='dropdown'>
-              <button className='btn btn-secondary dropdown-toggle' type='button' id='dropdownMenuButton' data-toggle='dropdown'>
-              Barrio
-              </button>
-              <div className='dropdown-menu' aria-labelledby='dropdownMenuButton'>
-                <a className='dropdown-item' href='#'>Action</a>
-                <a className='dropdown-item' href='#'>Another action</a>
-                <a className='dropdown-item' href='#'>Something else here</a>
+    changeAnio = (anio) => () => {
+      this.setState({
+        anio: anio
+      }, () => console.log(this.state.anio))
+    }
+
+    handleInputChange (evt) {
+      evt.preventDefault()
+      const { target: { value, name } } = evt
+
+      this.setState({ [name]: value })
+    }
+
+    render () {
+      return (
+        <div>
+          <section className='banner-static'>
+            <div className='banner gradient'>
+              <h1>Título de la sección</h1>
+              <h2>Descripción del contenido dentro de la sección</h2>
+            </div>
+          </section>
+          <div className='documentos-body'>
+            <div className='select-barrio'>
+              <p>Seleccioná el barrio que quieras visualizar</p>
+              <select
+                className='form-control'
+                required
+                name='barrio'
+                value={this.state['barrio']}
+                onChange={this.handleInputChange}>
+                <option value=''>Barrio</option>
+                <option value='villa-martelli'>Villa Marteli</option>
+                <option value='villa-adelina'>Villa Adelina</option>
+                <option value='vicente-lopez'>Vicente Lopez</option>
+                <option value='olivos'>Olivos</option>
+                <option value='munro'>Munro</option>
+                <option value='la-lucila'>La Lucila</option>
+                <option value='florida-oeste'>Florida Oeste</option>
+                <option value='florida-este'>Florida Este</option>
+                <option value='carapachay'>Carapachay</option>
+              </select>
+
+            </div>
+            <div className='timeline'>
+              <button onClick={this.changeAnio('2012')} >2012</button>
+              <button onClick={this.changeAnio('2013')} >2013</button>
+              <button onClick={this.changeAnio('2014')} >2014</button>
+              <button onClick={this.changeAnio('2015')} >2015</button>
+              <button onClick={this.changeAnio('2016')} >2016</button>
+              <button onClick={this.changeAnio('2017')} >2017</button>
+              <button onClick={this.changeAnio('2018')} >2018</button>
+
+              <div className='timeline-line'> { this.state.anio} { this.state.barrio}</div>
+
+            </div>
+
+            <div className='card'>
+              <div className='navbar-seccion'>
+                <div className='seccion is-active'>Minutas</div>
+                <div className='seccion'>Boletas</div>
+                <div className='seccion'>Proyectos</div>
               </div>
+              <div className='visualizador'></div>
             </div>
-
-          </div>
-          <div className='timeline'>
-            <div className='timeline-line'> 2008 </div>
           </div>
 
-          <div className='card'>
-            <div className='navbar-seccion'>
-              <div className='seccion'>Minutas</div>
-              <div className='seccion'>Boletas</div>
-              <div className='seccion'>Proyectos</div>
-            </div>
-            <div className='visualizador'></div>
-          </div>
+          <Footer />
         </div>
-
-        <Footer />
-      </div>
-    )
-  }
+      )
+    }
 }
