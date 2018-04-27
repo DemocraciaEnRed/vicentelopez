5d82361d0411665723c22505090865c3cdb8e649
florlafuente <lafuente.florencia@gmail.com>
2018-04-27 10:39:13 -0300

    Finish spinner styles

--- ext/lib/site/pdfviewer/component.js
+++ ext/lib/site/pdfviewer/component.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react'
 import PDF from 'react-pdf-js'
 
-const Loader = () => (
-  <div className='spinner-circle'>
-    <div className='spinner-circle1 spinner-child' />
-    <div className='spinner-circle2 spinner-child' />
-    <div className='spinner-circle3 spinner-child' />
-    <div className='spinner-circle4 spinner-child' />
-    <div className='spinner-circle5 spinner-child' />
-    <div className='spinner-circle6 spinner-child' />
-    <div className='spinner-circle7 spinner-child' />
-    <div className='spinner-circle8 spinner-child' />
-    <div className='spinner-circle9 spinner-child' />
-    <div className='spinner-circle10 spinner-child' />
-    <div className='spinner-circle11 spinner-child' />
-    <div className='spinner-circle12 spinner-child' />
-  </div>
-)
+class Loader extends Component {
+  render () {
+    return (
+      <div className='spinner-container'>
+        <div className='spinner-circle'>
+          <div className='spinner-circle1 spinner-child' />
+          <div className='spinner-circle2 spinner-child' />
+          <div className='spinner-circle3 spinner-child' />
+          <div className='spinner-circle4 spinner-child' />
+          <div className='spinner-circle5 spinner-child' />
+          <div className='spinner-circle6 spinner-child' />
+          <div className='spinner-circle7 spinner-child' />
+          <div className='spinner-circle8 spinner-child' />
+          <div className='spinner-circle9 spinner-child' />
+          <div className='spinner-circle10 spinner-child' />
+          <div className='spinner-circle11 spinner-child' />
+          <div className='spinner-circle12 spinner-child' />
+        </div>
+      </div>
+    )
+  }
+}
 
 export default class PdfViewer extends Component {
   constructor (props) {
@@ -75,7 +81,7 @@ export default class PdfViewer extends Component {
     return (
       <div>
         <PDF
-          fillWidth
+          fullWidth
           file={`//s3.amazonaws.com/forosvecinales/visualizador/${this.props.archivo}_${this.props.barrio}_${this.props.anio}.pdf`}
           onDocumentComplete={this.onDocumentComplete}
           onPageComplete={this.onPageComplete}
