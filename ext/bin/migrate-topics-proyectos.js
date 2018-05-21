4e94986f84147762c709b7ec427355977b0d2cbb
florlafuente <lafuente.florencia@gmail.com>
2018-05-21 11:24:33 -0300

    Fixes typo

--- ext/bin/migrate-topics-proyectos.js
+++ ext/bin/migrate-topics-proyectos.js
@@ -6,58 +6,50 @@ const ObjectID = require('mongodb').ObjectID
 const Forum = models.Forum
 const Topic = models.Topic
 
-// const forumsArray = [
-//   Forum.find({ 'name': { $in: barrios } }).exec(),
-//   Forum.find({ 'name': 'proyectos' }).exec()
-// ]
-
-// Promise.all(forumsArray)
-//   .then(([barrios, [ proyectos ]]) => {
-//     const barriosIds = barrios.map((b) => b.id)
-//     return Topic.find({ forum: { $in: barriosIds } })
-//       .then((topics) => {
-//         return {
-//           topics: topics,
-//           barrios: barrios,
-//           proyectos: proyectos
-//         }
-//       })
-//   })
-//   .then(({ topics, barrios, proyectos }) => {
-//     topics.map((topic) => {
-//       console.log(topic.attrs.description)
-//       console.log(typeof (topic.attrs.description))
-//       const barrioName = barrios.find((barrio) => {
-//         if (barrio.id === topic.forum.toString()) return barrio
-//       }).name
-//       const changeState = (state) => {
-//         let newState = null
-//         switch (state) {
-//           case 'pendiente':
-//             newState = 'factible'
-//             break
-//           case 'proyectado':
-//             newState = 'ganador'
-//             break
-//           case 'perdedor':
-//             newState = 'no-ganador'
-//         }
-//         return newState
-//       }
-//       topic.set('attrs.barrio', barrioName)
-//       topic.set('forum', proyectos.id)
-//       topic.set('attrs.state', changeState(topic.attrs.state))
-//       return topic.save()
-//     })
-//   })
-//   .catch((err) => console.log(err))
-
-const forums = [
+Promise.all([ Forum.find({ 'name': { $in: barrios } }).exec(), Forum.find({ 'name': 'proyectos' }).exec()])
+  .then(([barrios, [ proyectos ]]) => {
+    const barriosIds = barrios.map((b) => b.id)
+    return Topic.find({ forum: { $in: barriosIds } })
+      .then((topics) => {
+        return {
+          topics: topics,
+          barrios: barrios,
+          proyectos: proyectos
+        }
+      })
+  })
+  .then(({ topics, barrios, proyectos }) => {
+    topics.map((topic) => {
+      console.log(topic.attrs.description)
+      console.log(typeof (topic.attrs.description))
+      const barrioName = barrios.find((barrio) => {
+        if (barrio.id === topic.forum.toString()) return barrio
+      }).name
+      const changeState = (state) => {
+        let newState = null
+        switch (state) {
+          case 'pendiente':
+            newState = 'factible'
+            break
+          case 'proyectado':
+            newState = 'ganador'
+            break
+          case 'perdedor':
+            newState = 'no-ganador'
+        }
+        return newState
+      }
+      topic.set('attrs.barrio', barrioName)
+      topic.set('forum', proyectos.id)
+      topic.set('attrs.state', changeState(topic.attrs.state))
+      return topic.save()
+    })
+  })
+  
+Promise.all([
   Forum.find({ 'name': 'proyectos' }).exec(),
   Forum.find({ 'name': 'propuestas' }).exec()
-]
-
-Promise.all(forums)
+])
   .then(([[ proyectos ], [ propuestas ]]) => {
     return Topic.find({ 'forum': propuestas.id, 'attrs.state': 'factible' })
       .then((topics) => {
@@ -69,22 +61,19 @@ Promise.all(forums)
   })
   .then(({ topics, forum }) => {
     topics.map((topic) => {
-      const newDescription = () => {
+      topic.set('forum', forum.id)
+      topic.set('attrs.anio', 2018)
+      topic.set('attrs.budget', 0)
+      topic.set('attrs.votes', 0)
+      topic.set('attrs.description', () => {
         if (topic.attrs.solucion === undefined) {
           return topic.attrs.problema
         } else if (topic.attrs.solucion.length > 512) {
           return topic.attrs.solucion.substring(0, 509).concat('...')
         } else {
-          return topic.attrs.solcuion
+          return topic.attrs.solucion
         }
-      }
-      topic.set('forum', forum.id)
-      topic.set('attrs.anio', 2018)
-      topic.set('attrs.budget', 0)
-      topic.set('attrs.votes', 0)
-      topic.set('attrs.description', newDescription())
-      console.log(topic.attrs.description)
-      console.log(typeof (topic.attrs.description))
+      })
       return topic.save()
     })
   })
