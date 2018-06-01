5292c2547f526281c32e3e4d8beb4f0135bf68e1
florlafuente <lafuente.florencia@gmail.com>
2018-06-01 14:59:33 -0300

    Cambia numero propuestas

--- ext/lib/site/thumbs-voto/component.js
+++ ext/lib/site/thumbs-voto/component.js
@@ -10,7 +10,7 @@ export default function ThumbsVoto (props) {
             <h2>
               Presupuesto Participativo 2018
             </h2>
-            <h3>Gracias a la participación de vecinos e instituciones, recibimos 574 propuestas para los 9 barrios de Vicente López. Durante junio y julio analizaremos cuáles de ellas son posibles de realizar. En agosto, les informaremos cuáles de ellas participarán de la votación que se realizará entre el 24 de septiembre y el 7 de octubre de este año.</h3>
+            <h3>Gracias a la participación de vecinos e instituciones, recibimos 524 propuestas para los 9 barrios de Vicente López. Durante junio y julio analizaremos cuáles de ellas son posibles de realizar. En agosto, les informaremos cuáles de ellas participarán de la votación que se realizará entre el 24 de septiembre y el 7 de octubre de este año.</h3>
             <Link
               to='/propuestas'
               className="boton-azul boton-blanco">
