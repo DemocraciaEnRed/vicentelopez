caeec7be9fb149606b12f2207737e5db06a05d2b
luarar <laurarh.971@gmail.com>
2018-07-13 15:31:01 -0300

    agrega script de migracion del campo budget en proyectos

---
+++ ext/bin/migrate-pp-budget.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const models = require('lib/models')()
+const Topic = models.Topic
+
+Promise.all([
+  Topic.find({ 'attrs.anio': '2018' }).exec()
+])
+.then(([topics]) => {
+  
+  const newBudget = (state) => {
+    switch (state) {
+   case 'preparacion':
+   return 'attrs.project-budget-preparacion'
+   break
+   case 'compra':
+   return 'attrs.project-budget-compra'
+   break
+   case 'ejecucion':
+   return 'attrs.project-budget-ejecucion'
+   case 'finalizado':
+   return 'attrs.project-budget-finalizado'
+   }
+   }  
+   const newTopics = topics.map((topic) => {
+    topic.set(changeBudget(topic.attrs.state), topic.attrs.budget)
+    return topic.save()
+  })
+
+  return Promise.all([...newTopics])
+})
+.then((newTopics) => {
+  console.log('Proyectos actualizados!')
+  process.exit(0)
+})
+.catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
