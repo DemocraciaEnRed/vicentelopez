9dd9e943c9563a90b5f20fbff7ad558db4de275d
luarar <laurarh.971@gmail.com>
2018-07-16 14:52:45 -0300

    actualiza topics

--- ext/bin/migrate-pp-budget.js
+++ ext/bin/migrate-pp-budget.js
@@ -6,28 +6,88 @@ const Topic = models.Topic
 Promise.all([
   Topic.find({ 'attrs.anio': '2018' }).exec()
 ])
+.then(([topics2018]) => {
+  const newBudgetState = topics2018.map((topic) => {
+
+    const changeState = (state) => {
+      let newState = null
+      switch (state) {
+        case 'preparacion':
+        newState = 'project-budget-preparacion'
+        break
+        case 'compra':
+        newState = 'project-budget-compra'
+        break
+        case 'ejecucion':
+        newState = 'project-budget-ejecucion'
+        break
+        case 'finalizado':
+        newState = 'project-budget-finalizado'
+      }
+      return newState
+    }
+    const newTopics = topics.map((topic) => {
+      topic.set(changeState(topic.attrs.state), topic.attrs.budget)
+      return topic.save()
+    })
+  })
+  
+  
+  return Promise.all([...newBudgetState])
+})
+.then((newTopics) => {
+  console.log('Topics actualizados!')
+  process.exit(0)
+})
+.catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
+
+
+
+
+
+
+
+
+
+
+
+/*
+const models = require('lib/models')()
+const Topic = models.Topic
+
+Promise.all([
+  Topic.find({ 'attrs.anio': '2018' }).exec()
+])
 .then(([topics]) => {
   
   const newBudget = (state) => {
+    let newState = null
     switch (state) {
    case 'preparacion':
-   return 'attrs.project-budget-preparacion'
+   newState = 'attrs.project-budget-preparacion'
    break
    case 'compra':
-   return 'attrs.project-budget-compra'
+   newState = 'attrs.project-budget-compra'
    break
    case 'ejecucion':
-   return 'attrs.project-budget-ejecucion'
+   newState = 'attrs.project-budget-ejecucion'
+   break
    case 'finalizado':
-   return 'attrs.project-budget-finalizado'
+   newState = 'attrs.project-budget-finalizado'
    }
+   return newState
+
    }  
+
    const newTopics = topics.map((topic) => {
     topic.set(newBudget(topic.attrs.state), topic.attrs.budget)
     return topic.save()
   })
 
-  return Promise.all([...newTopics])
+  return Promise.all(newTopics)
 })
 .then((newTopics) => {
   console.log('Proyectos actualizados!')
@@ -37,3 +97,5 @@ Promise.all([
     console.error(err)
     process.exit(1)
   })
+
+  */
