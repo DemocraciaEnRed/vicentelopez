02bfeb06910d2efd87696c90faa1859fb78a6270
luarar <laurarh.971@gmail.com>
2018-07-18 11:11:03 -0300

    script de migracion funcionando

--- ext/bin/migrate-pp-budget.js
+++ ext/bin/migrate-pp-budget.js
@@ -3,40 +3,42 @@
 const models = require('lib/models')()
 const Topic = models.Topic
 
-
 const changeState = (state) => {
-  let newState = null
   switch (state) {
     case 'preparacion':
-    newState = 'project-budget-preparacion'
-    break
+      return 'attrs.project-budget-preparacion'
+      break
     case 'compra':
-    newState = 'project-budget-compra'
-    break
+     return 'attrs.project-budget-compra'
+      break
     case 'ejecucion':
-    newState = 'project-budget-ejecucion'
-    break
+    return 'attrs.project-budget-ejecucion'
+      break
     case 'finalizado':
-    newState = 'project-budget-finalizado'
+      return 'attrs.project-budget-finalizado'
+    default:
+      return 'undefined'  
   }
-  return newState
 }
 
-Promise.all([
-  Topic.find({ 'attrs.anio': '2018' }).exec()
-])
-.then(([topics2018]) => {
-  const newBudgetState = topics2018.map((topic) => {
+Topic.find({ 'attrs.anio': '2018' }).exec()
+.then((topics) => {
     const newTopics = topics.map((topic) => {
-      topic.set(changeState(topic.attrs.state), topic.attrs.budget)
-      return topic.save()
+   
+      const budgetField = changeState(topic.attrs.state)
+      console.log('////////////////////')
+      console.log(budgetField)
+      console.log('////////////////////')
+      if (budgetField !='undefined'){
+        topic.set(budgetField, topic.attrs.budget)
+        return topic.save()
+      }
     })
-  })
   
-  
-  return Promise.all([...newBudgetState])
+  return Promise.all(newTopics)
 })
 .then((newTopics) => {
+  console.log(newTopics)
   console.log('Topics actualizados!')
   process.exit(0)
 })
@@ -44,59 +46,3 @@ Promise.all([
     console.error(err)
     process.exit(1)
   })
-
-
-
-
-
-
-
-
-
-
-
-/*
-const models = require('lib/models')()
-const Topic = models.Topic
-
-Promise.all([
-  Topic.find({ 'attrs.anio': '2018' }).exec()
-])
-.then(([topics]) => {
-  
-  const newBudget = (state) => {
-    let newState = null
-    switch (state) {
-   case 'preparacion':
-   newState = 'attrs.project-budget-preparacion'
-   break
-   case 'compra':
-   newState = 'attrs.project-budget-compra'
-   break
-   case 'ejecucion':
-   newState = 'attrs.project-budget-ejecucion'
-   break
-   case 'finalizado':
-   newState = 'attrs.project-budget-finalizado'
-   }
-   return newState
-
-   }  
-
-   const newTopics = topics.map((topic) => {
-    topic.set(newBudget(topic.attrs.state), topic.attrs.budget)
-    return topic.save()
-  })
-
-  return Promise.all(newTopics)
-})
-.then((newTopics) => {
-  console.log('Proyectos actualizados!')
-  process.exit(0)
-})
-.catch((err) => {
-    console.error(err)
-    process.exit(1)
-  })
-
-  */
