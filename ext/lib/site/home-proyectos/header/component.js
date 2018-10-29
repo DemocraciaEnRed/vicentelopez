b844268db53b2c69fefc15cf2ad819a90498b366
florlafuente <lafuente.florencia@gmail.com>
2018-10-29 14:31:35 -0300

    Cambia titulo y subtitulo en vista proyectos stage votacion-abierta

--- ext/lib/site/home-proyectos/header/component.js
+++ ext/lib/site/home-proyectos/header/component.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default ({ stage }) => (
   <header className='banner-proyectos'>
-    <h1 className='proyectos-title'>{stage === 'votacion' ? 'Votación de proyectos' : 'Seguimiento de proyectos' }</h1>
-    <h2 className='proyectos-subtitle'>{stage === 'votacion' ? 'Acá podes encontrar los proyectos a ser votados...' : 'Acá podés encontrar los proyectos que fueron aprobados en votaciones anteriores y ver en qué estado de su ejecución se encuentran.'}</h2>
+    <h1 className='proyectos-title'>{stage === 'votacion' ? 'Proyectos ganadores' : 'Seguimiento de proyectos' }</h1>
+    <h2 className='proyectos-subtitle'>{stage === 'votacion' ? 'Acá podes encontrar los proyectos ganadores de 2018' : 'Acá podés encontrar los proyectos que fueron aprobados en votaciones anteriores y ver en qué estado de su ejecución se encuentran.'}</h2>
   </header>
 )
