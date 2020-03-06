30872bf3c883e08babc5ac7829bf3daa309a71d1
wencha <wencha_@hotmail.com>
2020-03-06 16:03:02 -0300

    Fix usando nuevo config.store: videos de portada en home no andaban

--- ext/lib/site/banner-foro-vecinal/component.js
+++ ext/lib/site/banner-foro-vecinal/component.js
@@ -1,26 +1,28 @@
 import React from 'react'
+import config from 'lib/config'
 
 export default function BannerForoVecinal (props) {
+  let { adminTexts } = config.store
   return (
     <section className='container-fluid banner-fv'>
-            <div className='blue-background'></div>
-
+      <div className='blue-background'></div>
       <div className='banner'>
-        {
+      {
         Math.random(1) > 0.5 ? (
           window.innerWidth >= 630 &&
             <video playsInline autoPlay muted loop>
-              <source src={ props.texts['home-video1-webm'] } type='video/webm' />
-              <source src={ props.texts['home-video1-mp4'] }  type='video/mp4' />
+              <source src={ adminTexts['home-video1-webm'] } type='video/webm' />
+              <source src={ adminTexts['home-video1-mp4'] }  type='video/mp4' />
             </video>
         ) : (
           window.innerWidth >= 630 &&
             <video playsInline autoPlay muted loop>
-              <source src={ props.texts['home-video2-webm'] } type='video/webm' />
-              <source src={ props.texts['home-video2-mp4'] } type='video/mp4' />
+              <source src={ adminTexts['home-video2-webm'] } type='video/webm' />
+              <source src={ adminTexts['home-video2-mp4'] } type='video/mp4' />
             </video>
         )
-        }
+      }
+      
       </div>
       <div className='contenedor'>
         <div className='fondo-titulo'>
