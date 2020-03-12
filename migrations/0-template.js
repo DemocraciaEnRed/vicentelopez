0816b5e2dafbb671b52181435ce6e5ded46ba10b
wencha <wencha_@hotmail.com>
2020-03-12 17:09:31 -0300

    Agregado template de migration

---
+++ migrations/0-template.js
@@ -0,0 +1,35 @@
+const dbReady = require('lib/models').ready
+const models = require('lib/models')
+
+const nombreMigrationParaLog = 'textos de portada'
+const Topic = models.Topic
+
+/**
+ * Make any changes you need to make to the database here
+ */
+exports.up = function up (done) {
+  // done() devuelve al Migrator de lib/migrations
+  dbReady()
+
+    // Código principal de migración
+    .then(() => Topic.find())
+    .then((topics) => hacerAlgo)
+
+    // Todo OK
+    .then(() => {
+      console.log(`-- Migración ${nombreMigrationParaLog} exitosa`)
+      done()
+    })
+    // Error
+    .catch((err) => {
+      console.log(`-- Migración ${nombreMigrationParaLog} no funcionó! Error: ${err}`)
+      done(err)
+    })
+}
+
+/**
+ * Make any changes that UNDO the up function side effects here (if possible)
+ */
+exports.down = function down(done) {
+  done();
+};
