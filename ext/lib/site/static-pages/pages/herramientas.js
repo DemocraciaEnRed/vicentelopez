c1fa192d7dd7930137ef449d459709c492c496d9
wencha <wencha_@hotmail.com>
2020-03-13 15:56:20 -0300

    Ocultar temporalmente flyer de reuniones informativas en herramientas

--- ext/lib/site/static-pages/pages/herramientas.js
+++ ext/lib/site/static-pages/pages/herramientas.js
@@ -63,14 +63,14 @@ export default class Page extends Component {
                 </div>
               </div>
             </div> */}
-            <img className="flyer-pp" src='/ext/lib/site/static-pages/flyer-reuniones-pp-2020.jpg' alt="Flyer reuniones del presupuesto participativo 2020"/>
+            {/*<img className="flyer-pp" src='/ext/lib/site/static-pages/flyer-reuniones-pp-2020.jpg' alt="Flyer reuniones del presupuesto participativo 2020"/>
             <div className="fila no-bg">
               <div className="map-box">
                 <div className='mapa'>
                   <iframe src="https://www.google.com/maps/d/embed?mid=1qhRpeylCuWIO7llVNSi5prF1JDBuI13b" width="640" height="480"></iframe>
                 </div>
               </div>
-            </div>
+            </div>*/}
 
             {/* <img controls className='mapa-municipio' src='https://forosvecinales.blob.core.windows.net/assets/mapa_vicente-lopez.jpg' /> */}
           </div>
