1b3c6e2537451d57f96b9db3a59746a65f367d4c
mijail cohen <mijaelcohen@gmail.com>
2021-08-25 16:10:52 -0300

    banner uggly

--- ext/lib/site/banner-foro-vecinal/component.js
+++ ext/lib/site/banner-foro-vecinal/component.js
@@ -3,24 +3,19 @@ import config from 'lib/config'
 
 export default function BannerForoVecinal (props) {
   let { adminTexts } = config.store
+  let random  = Math.random(1) > 0.5 ? "1":"2";
+  let source = `home-video${random}`;
+  //DRY: dont repeat yourself
   return (
     <section className='container-fluid banner-fv'>
       <div className='blue-background'></div>
       <div className='banner'>
       {
-        Math.random(1) > 0.5 ? (
-          window.innerWidth >= 630 &&
-            <video playsInline autoPlay muted loop>
-              <source src={ adminTexts['home-video1-webm'] } type='video/webm' />
-              <source src={ adminTexts['home-video1-mp4'] }  type='video/mp4' />
-            </video>
-        ) : (
-          window.innerWidth >= 630 &&
-            <video playsInline autoPlay muted loop>
-              <source src={ adminTexts['home-video2-webm'] } type='video/webm' />
-              <source src={ adminTexts['home-video2-mp4'] } type='video/mp4' />
-            </video>
-        )
+        window.innerWidth >= 630 &&
+          <video playsInline autoPlay muted loop>
+            <source src={ adminTexts[`${source}-webm`] } type='video/webm' />
+            <source src={ adminTexts[`${source}-mp4`] }  type='video/mp4' />
+          </video>
       }
 
       </div>
