571233f9735c5be7027c63ee4bd67a4d2f091ad4
Guillermo Croppi <guillermocroppi@gmail.com>
2021-09-20 13:25:21 -0300

    Cambios acerca de

--- ext/lib/site/static-pages/pages/acerca-de.js
+++ ext/lib/site/static-pages/pages/acerca-de.js
@@ -34,11 +34,11 @@ export default class Page extends Component {
               <div className="fila faq text-left">
                 <Accordion startPosition={-1}>
                   <div data-trigger="+ ¿QUÉ ES EL PRESUPUESTO PARTICIPATIVO DE VICENTE LÓPEZ?">
-                    <p className='p-padding'>El Presupuesto Participativo de Vicente López es un espacio de participación a través del cual los vecinos proponen y deciden en qué invertir una parte del presupuesto municipal. Todos los años, y desde hace 9 años, se asigna y comunica una partida por barrio para la ejecución de estos proyectos. </p>
+                    <p className='p-padding'>El Presupuesto Participativo de Vicente López (PPVL) es un espacio de participación a través del cual los habitantes de Vicente López proponen y deciden en qué invertir una parte del presupuesto municipal. Todos los años, y desde hace 10 años, se asigna y comunica una partida por barrio para la ejecución de estos proyectos.</p>
                   </div>
 
                    <div data-trigger="+ ¿CÓMO SE DISTRIBUYE EL DINERO POR BARRIO?">
-                      <p className='p-padding'>La partida asignada al Programa se distribuye según dos criterios: el 50% por partes iguales a cada uno de los 9 barrios. Y el otro 50% según la cantidad de habitantes de cada barrio. Así, los barrios con más población, reciben más partida presupuestaria.</p>
+                      <p className='p-padding'>La partida asignada al Programa se distribuye según dos criterios: aproximadamente el 50% por partes iguales a cada uno de los 9 barrios. Y el resto, según la cantidad de habitantes de cada barrio. Así, los barrios con más población, reciben más partida presupuestaria.</p>
                       <p>Para el 2022, el presupuesto participativo será de $200.000.000 y se distribuye así: </p>
                       <ul>
                         <li><p><b>OLIVOS</b>: $ 26.208.378</p></li>
@@ -54,7 +54,7 @@ export default class Page extends Component {
                    </div>
 
                    <div data-trigger="+ ¿QUIÉN PUEDE PRESENTAR PROPUESTAS?">
-                   <p>Cualquier persona mayor de 16 años que viva o trabaje en cualquiera de los 9 barrios de Vicente López. También pueden presentar propuestas las instituciones de bien público registradas en el Partido.</p>
+                   <p>Cualquier persona mayor de 16 años que viva o trabaje en cualquiera de los 9 barrios de Vicente López. También pueden presentar propuestas las instituciones de bien público registradas en el Partido y las instituciones educativas de gestión estatal de la Provincia de Buenos Aires con sede en el Partido de Vicente López.</p>
                    </div>
 
                    <div data-trigger="+ ¿CUÁNTOS PROYECTOS PUEDE PRESENTAR UNA ENTIDAD?">
@@ -62,16 +62,16 @@ export default class Page extends Component {
                    </div>
 
                    <div data-trigger="+ ¿CÓMO SE ELABORAN LOS PROYECTOS?">
-                   <p>En una serie de reuniones informativas, los vecinos y los representantes de las entidades se encuentran con funcionarios municipales para presentarles propuestas para cada barrio. Las propuestas luego se cargan en esta plataforma.</p>
+                   <p>En una serie de reuniones informativas, los vecinos y los representantes de las entidades se encuentran con funcionarios municipales para presentarles propuestas para cada barrio. Las propuestas siempre deben cargarse en esta plataforma, utilizando el formulario de presentación de propuestas.</p>
                    </div>
 
                    <div data-trigger="+ ¿QUÉ TIPO DE PROYECTOS PUEDO PRESENTAR?">
                     <p>Podés presentar:</p>
                     <p>1. Propuestas de obras o equipamiento para: espacios públicos, escuelas de gestión pública, centros de salud municipales, polideportivos, sociedades de fomento, centros de jubilados, sociedades de fomento, clubes, entre otros. La condición es que sean entidades sin fines de lucro.</p>
-                    <p>2. También podés presentar propuestas de campañas o talleres sobre un determinado tema. En ese caso, se limitarán exclusivamente al año de ejecución que le corresponda a esa propuesta, en caso de que los vecinos la elijan.</p>
+                    <p>2. También propuestas de campañas o talleres sobre un determinado tema. En ese caso, se limitarán exclusivamente al año de ejecución que le corresponda a esa propuesta, en caso de que los vecinos la elijan.</p>
                     <p>3. Las propuestas no pueden involucrar gastos en recursos humanos que incrementen la planta de empleados municipales.</p>
                     <p>4. Tenés que acotarla a un barrio (localidad) en concreto. No puede ser una propuesta para todo el Municipio.</p>
-                    <p>5. El presupuesto máximo de la propuesta no puede superar un monto determinado (para los proyectos a ejecutar en 2022 será de $ 5.000.000).</p>
+                    <p>5. El presupuesto máximo de la propuesta no puede superar un monto determinado (para los proyectos a ejecutar en 2022 será de $5.000.000).</p>
                     <p>6. La propuesta no puede referirse a un espacio que no esté en jurisdicción del Municipio de Vicente López, a excepción de las escuelas públicas de gestión provincial.</p>
                    </div>
 
@@ -81,8 +81,8 @@ export default class Page extends Component {
 
                    <div data-trigger="+ ¿QUIÉN PUEDE VOTAR LOS PROYECTOS?">
                      <p>Cualquier mayor de 16 años que viva o trabaje en Vicente López puede votar los proyectos. Para ello, tenés que acreditar domicilio particular o laboral en alguno de los 9 barrios del Partido.</p>
-                     <p>En el caso de los proyectos de escuelas públicas de gestión provincial o municipal, también pueden votar los alumnos mayores de 16 años y hasta un mayor de 16 años por cada alumno menor de 16 años inscriptos en la escuela respectiva, aunque no tengan domicilio en el barrio o en el partido de Vicente López. Estas personas solo podrán elegir el proyecto de la escuela y deberán estar registradas en un padrón especial que debe confeccionar la escuela y y cargar en la plataforma que le indique la Subsecretaría de Participación Ciudadana oportunamente.</p>
-                     <p>En el caso de proyectos presentados por entidades intermedias, sus directivos, asociados e inscriptos en talleres brindados por las mismas que no tengan domicilio en el barrio donde esté ubicada la entidad o en el partido de Vicente López también podrán votar pero solo podrán elegir el proyecto de la entidad y deberán estar registrados en un padrón especial que debe confeccionar la entidad y y cargar en la plataforma que le indique la Subsecretaría de Participación Ciudadana oportunamente.</p>
+                     <p>En el caso de los proyectos de escuelas públicas de gestión provincial o municipal, también pueden votar los alumnos mayores de 16 años y hasta un mayor de 16 años por cada alumno menor de 16 años inscriptos en la escuela respectiva, aunque no tengan domicilio en el barrio o en el partido de Vicente López. Estas personas solo podrán elegir el proyecto de la escuela y deberán estar registradas en un padrón especial que debe confeccionar la escuela y cargar en la plataforma que le indique la Subsecretaría de Participación Ciudadana oportunamente.</p>
+                     <p>En el caso de proyectos presentados por entidades intermedias, sus directivos, asociados e inscriptos en talleres brindados por las mismas que no tengan domicilio en el barrio donde esté ubicada la entidad o en el partido de Vicente López también podrán votar pero solo podrán elegir el proyecto de la entidad y deberán estar registrados en un padrón especial que debe confeccionar la entidad y cargar en la plataforma que le indique la Subsecretaría de Participación Ciudadana oportunamente.</p>
                    </div>
 
                    <div data-trigger="+ ¿CÓMO ACREDITO DOMICILIO?">
@@ -96,31 +96,39 @@ export default class Page extends Component {
                    </div>
 
                    <div data-trigger="+ ¿POR QUÉ PUEDEN PARTICIPAR CON PROPUESTAS LAS ESCUELAS PÚBLICAS DE GESTIÓN PROVINCIAL EN EL PRESUPUESTO PARTICIPATIVO?">
-                    <p>Si bien las escuelas públicas de gestión provincial que funcionan en Vicente López dependen presupuestariamente de la Provincia de Buenos Aires, desde el año 2014 y dado el estado de las mismas, el Departamento Ejecutivo Municipal permite que dichas escuelas presenten propuestas al Presupuesto Participativo. Para ello, se estableció un monto límite por barrio y se plantearon 2 requisitos para acceder a este presupuesto: 1) que la escuela presente la propuesta y 2) que los vecinos del barrio la elijan con su voto.</p>
+                    <p>Si bien las escuelas públicas de gestión provincial que funcionan en Vicente López dependen presupuestariamente de la Provincia de Buenos Aires, desde el año 2014 y dado el estado de las mismas, el Departamento Ejecutivo Municipal permite que dichas escuelas presenten propuestas al Presupuesto Participativo. Para ello, se estableció un monto límite por barrio ($5.000.000) y se plantearon 2 requisitos: Para presentar la propuesta, la escuela debe contar con la aprobación del Consejo Escolar y, de resultar un proyecto factible, debe ganarse el derecho a ser realizado en igualdad de condiciones que el resto de los proyectos, es decir, a través del voto directo de las personas habilitadas para votar.</p>
                    </div>
 
-                   <div data-trigger="+ ¿CUÁNDO Y DÓNDE VOTO?">
-                   <p>La votación se realiza entre los meses de septiembre y octubre de cada año. Si las condiciones sanitarias lo permiten, encontrarás urnas habilitadas para votar en los 9 barrios de Vicente López: Munro, Florida Oeste, Carapachay, La Lucila, Olivos, Villa Martelli, Florida Este, Vicente López y Villa Adelina. En 2021, incorporaremos la modalidad de voto online.</p>
+                   <div data-trigger="+ ¿CUÁNDO Y DÓNDE VOTAR?">
+                   <p>En 2021, la votación se realizará exclusivamente por modalidad online en un portal al que se accede por esta misma plataforma web. Las personas registradas y habilitadas para votar podrán hacerlo entre el lunes 18 de octubre a las 00.00 hs y el domingo 31 de octubre a las 23.59 hs</p>
                    </div>
 
-                   <div data-trigger="+ ¿CÓMO VOTO?">
-                   <p>Si podemos realizar votación presencial en 2021, te acercás a los puntos de votación, completás tus datos en la boleta del barrio correspondiente, marcás los 3 proyectos que considerás prioritarios para tu barrio y listo. También tendrás la posibilidad de votar online, registrándote en nuestra página web unas semanas antes de que comience la votación.</p>
+                   <div data-trigger="+ ¿SE PUEDE VOTAR MÁS DE UNA VEZ?">
+                   <p>Solo podés votar más de una vez si vivís en uno de los 9 barrios y trabajás en otro de los 9 barrios. Si vivís y trabajás en el mismo barrio, sólo podés votar una vez.</p>
+                   </div>
+
+                   <div data-trigger="+ ¿CÓMO VOTAR?">
+                   <p>Para poder votar los proyectos, antes tenés que registrarte en el mismo portal de votación: <a href="https://votacionpp.vicentelopez.gov.ar/pre-registro" target="_blank">https://votacionpp.vicentelopez.gov.ar/pre-registro</a>. La etapa de registración para votar estará habilitada al menos un mes antes de la votación. Las personas que vivan y/o trabajen en Vicente López podrán registrarse desde el 1 de septiembre de 2021 hasta el 31 de octubre de 2021.</p>
+                   </div>
+                   
+                   <div data-trigger="+ ¿QUÉ DOCUMENTACIÓN NECESITO PARA REGISTRARME?">
+                   <p>Para registrarte para votar los proyectos, deberás tener a mano tu DNI con domicilio en Vicente López.</p>
                    </div>
 
                    <div data-trigger="+ ¿CUÁNTOS PROYECTOS PUEDO ELEGIR?">
-                   <p>Podés elegir hasta 3 proyectos, tildandolos en la boleta de votación del barrio correspondiente. Si elegís más de 3 proyectos, el voto es nulo.</p>
+                   <p>Podés elegir hasta 3 proyectos, tildándolos en el listado de proyectos de los barrios en los que estés habilitado para votar según tu registro.</p>
                    </div>
 
                    <div data-trigger="+ ¿EL VOTO ES SECRETO?">
-                   <p>No, el voto en el Presupuesto Participativo es nominal. Como no existe un empadronamiento previo para votar, en el momento del escrutinio necesitamos poder verificar 1) que no haya duplicaciones de DNI, 2) que el domicilio que acredita la persona es del barrio, para que el voto sea válido.</p>
+                   <p>No, el voto en el Presupuesto Participativo de Vicente López es nominal.</p>
                    </div>
 
                    <div data-trigger="+ ¿CUÁNDO SE CUENTAN LOS VOTOS?">
-                   <p>El escrutinio de cada votación se realiza durante los 4 subsiguientes a la finalización de la votación en lugar a designar por la Subsecretaría de Participación Ciudadana. </p>
+                   <p>El escrutinio se realizará el lunes 1 de noviembre de 2021 en lugar a designar por la Subsecretaría de Participación Ciudadana.</p>
                    </div>
 
                    <div data-trigger="+ ¿SE PUEDE PRESENCIAR EL ESCRUTINIO?">
-                   <p>Por supuesto. El escrutinio es público y abierto a todos los vecinos de Vicente López. Si te interesa presenciar el escrutinio de un barrio en particular, mandanos un mail a <a href="mailto:participacion.ciudadana@vicentelopez.gov.ar">participacion.ciudadana@vicentelopez.gov.ar</a> y te avisamos qué día y en qué horario se contarán los votos de las urnas ubicadas en los puntos de votación de ese barrio.</p>
+                   <p>Por supuesto. El escrutinio es público. Si te interesa presenciarlo, mandanos un mail a <a href="mailto:participacion.ciudadana@vicentelopez.gov.ar">participacion.ciudadana@vicentelopez.gov.ar</a></p>
                    </div>
 
                    <div data-trigger="+ ¿CÓMO SE DEFINEN LOS PROYECTOS GANADORES?">
@@ -128,7 +136,7 @@ export default class Page extends Component {
                    </div>
 
                    <div data-trigger="+ ¿QUÉ HAGO SI TENGO UNA PREGUNTA QUE NO SE HA RESPONDIDO AQUÍ?">
-                   <p>Mandanos un WhatsApp al +5491162426741 o envianos un mail a <a href="mailto:participacion.ciudadana@vicentelopez.gov.ar">participacion.ciudadana@vicentelopez.gov.ar</a> y te responderemos a la brevedad.</p>
+                   <p>Consultá el reglamento del PPVL para 2021-2022 haciendo <a href='https://celeste.blob.core.windows.net/pp-vicentelopez/assets/reglamento-pp-vicente-lopez.pdf' target='_blank'>click acá</a>, envianos un WhatsApp al +5491162426741 o envianos un mail a <a href="mailto:participacion.ciudadana@vicentelopez.gov.ar">participacion.ciudadana@vicentelopez.gov.ar</a> y te responderemos a la brevedad.</p>
                    </div>
 
                    <div data-trigger="+ ¿DÓNDE ENCUENTRO EL REGLAMENTO DEL PRESPUESTO PARTICIPATIVO 2021?">
