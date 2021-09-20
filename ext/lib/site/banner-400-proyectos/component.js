12ab02a85d7aef94e07b81ef7e3ebed2251b9bda
Guillermo Croppi <guillermocroppi@gmail.com>
2021-09-20 13:25:14 -0300

    400 a 500 proyectos

--- ext/lib/site/banner-400-proyectos/component.js
+++ ext/lib/site/banner-400-proyectos/component.js
@@ -6,7 +6,7 @@ export default function Banner400Proyectos(props) {
       <div className="container">
         <a href="https://400proyectos.vicentelopez.gob.ar" target="_blank">
           <h2 className="festejando-title">
-            ¡FESTEJAMOS +400 PROYECTOS EJECUTADOS!
+            ¡FESTEJAMOS +500 PROYECTOS EJECUTADOS!
             </h2>
           <p className="festejando-p">¡Conocé todo lo que hicimos juntos!</p>
           <p className="festejando-p smaller">https://400proyectos.vicentelopez.gob.ar</p>
